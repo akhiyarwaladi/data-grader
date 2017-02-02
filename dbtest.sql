27fc946be378ffd92934073b634947c90e62c4a6
akhiyarwaladi <akiyar18@gmail.com>
2017-02-02 07:37:20 +0700

    tambah soal

--- dbtest.sql
+++ dbtest.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 01, 2017 at 07:27 PM
+-- Generation Time: Feb 02, 2017 at 01:36 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -43,7 +43,9 @@ INSERT INTO `data` (`dataId`, `name`, `description`, `leaderboard`, `link`, `ima
 (1, 'Warm Up: Predict Blood Donations', 'Blood donation has been around for a long time. The first successful recorded transfusion was between two dogs in 1665, and the first medical use of human blood in a transfusion occurred in 1818. Even today, donated blood remains a critical resource during emergencies.\\n\\nOur dataset is from a mobile blood donation vehicle in Taiwan. The Blood Transfusion Service Center drives to different universities and collects blood as part of a blood drive. We want to predict whether or not a donor will give blood the next time the vehicle comes to campus.', 'ayase', 'https://goo.gl/AMWiLw', '1.jpg'),
 (2, 'Pump it Up: Data Mining the Water Table', 'Can you predict which water pumps are faulty?\\n\\nUsing data from Taarifa and the Tanzanian Ministry of Water, can you predict which pumps are functional, which need some repairs, and which don''t work at all? This is an intermediate-level practice competition. Predict one of these three classes based on a number of variables about what kind of pump is operating, when it was installed, and how it is managed. A smart understanding of which waterpoints will fail can improve maintenance operations and ensure that clean, potable water is available to communities across Tanzania.', 'ayase', 'https://goo.gl/MGyvAB', '2.jpg'),
 (3, 'Mushroom Classification', 'This dataset includes descriptions of hypothetical samples corresponding to 23 species of gilled mushrooms in the Agaricus and Lepiota Family Mushroom drawn from The Audubon Society Field Guide to North American Mushrooms (1981). Each species is identified as definitely edible, definitely poisonous, or of unknown edibility and not recommended. This latter class was combined with the poisonous one. The Guide clearly states that there is no simple rule for determining the edibility of a mushroom; no rule like "leaflets three, let it be'''' for Poisonous Oak and Ivy.\r\n', 'ayase', 'https://goo.gl/7PSyva', '3.jpg'),
-(4, 'Digit Recognizer', 'A popular demonstration of the capability of deep learning techniques is object recognition in image data. The hello world of object recognition for machine learning and deep learning is\r\nthe MNIST dataset for handwritten digit recognition.\\n\\nThe MNIST problem is a dataset developed by Yann LeCun, Corinna Cortes and Christopher Burges for evaluating machine learning models on the handwritten digit classification problem.\\n\\n The dataset was constructed from a number of scanned document datasets available from the\r\nNational Institute of Standards and Technology (NIST). This is where the name for the dataset comes from, as the Modified NIST or MNIST dataset.', 'NotYet', 'https://goo.gl/iDRfop', '4.gif');
+(4, 'Digit Recognizer', 'A popular demonstration of the capability of deep learning techniques is object recognition in image data. The hello world of object recognition for machine learning and deep learning is\r\nthe MNIST dataset for handwritten digit recognition.\\n\\nThe MNIST problem is a dataset developed by Yann LeCun, Corinna Cortes and Christopher Burges for evaluating machine learning models on the handwritten digit classification problem.\\n\\n The dataset was constructed from a number of scanned document datasets available from the\r\nNational Institute of Standards and Technology (NIST). This is where the name for the dataset comes from, as the Modified NIST or MNIST dataset.', 'NotYet', 'https://goo.gl/iDRfop', '4.gif'),
+(5, 'Pima Indians Diabetes', 'This dataset is originally from the National Institute of Diabetes and Digestive and Kidney Diseases. The objective is to predict based on diagnostic measurements whether a patient has diabetes.\\n\\nDataset information\\nSeveral constraints were placed on the selection of these instances from a larger database. In particular, all patients here are females at least 21 years old of Pima Indian heritage.', 'NotYet', 'https://goo.gl/TXXzYq', '5.jpeg'),
+(6, 'Forest Cover Type Dataset', 'This dataset contains tree observations from four areas of the Roosevelt National Forest in Colorado. All observations are cartographic variables (no remote sensing) from 30 meter x 30 meter sections.\\n\\nThis dataset is part of the UCI Machine Learning Repository, and the original source can be found here. The original database owners are Jock A. Blackard, Dr. Denis J. Dean, and Dr. Charles W. Anderson of the Remote Sensing and GIS Program at Colorado State University.', 'NotYet', 'https://goo.gl/sc8iIN', '6.jpeg');
 
 -- --------------------------------------------------------
 
@@ -123,7 +125,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
-  MODIFY `dataId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `dataId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `score`
 --
